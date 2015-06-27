597befca52d1f44671449a9c8e668633244c9d98
kazunari-h <kazunario456@gmail.com>
2015-06-27 14:02:28 +0900

    first commit

---
+++ ResMap/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ResMap
+//
+//  Created by hirosawak on 2015/06/27.
+//  Copyright (c) 2015年 hirosawak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
